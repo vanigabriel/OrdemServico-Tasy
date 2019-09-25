c1c539f155c327384f050439c0dcdb70e99d91ea
Gabriel Angelo Vani <vani.gabr@gmail.com>
2019-09-25 12:03:33 -0300

    fix reflection tag

--- entity/ordemservico.go
+++ entity/ordemservico.go
@@ -1,7 +1,7 @@
 package entity
 
 type OrdemServico struct {
-	NrCPF     string `json:"nrCPF" valide:"required"`
-	Descricao string `json:"damage" valide:"required"`
-	Contato   string `json:"contato" valide:"required"`
+	NrCPF     string `json:"nrCPF" valid:"required"`
+	Descricao string `json:"damage" valid:"required"`
+	Contato   string `json:"contato" valid:"required"`
 }
