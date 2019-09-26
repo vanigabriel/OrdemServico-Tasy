c859c670505a381841f283d075384da7e14dd6df
Gabriel Angelo Vani <vani.gabr@gmail.com>
2019-09-26 09:46:39 -0300

    Update api.go

--- api/api.go
+++ api/api.go
@@ -1,8 +1,12 @@
 package api
 
 import (
+	"io"
+	"io/ioutil"
 	"log"
+	"mime"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/vanigabriel/OrdemServico-Tasy/entity"
@@ -39,39 +43,43 @@ func PostOS(service *ordem.Service) func(c *gin.Context) {
 		}
 
 		multipart, err := c.Request.MultipartReader()
-	if err != nil {
-		log.Fatalln("Failed to create MultipartReader", err)
-	}
-
-	for {
-		mimePart, err := multipart.NextPart()
-		if err == io.EOF {
-			break
-		}
 		if err != nil {
-			log.Printf("Error reading multipart section: %v", err)
-			break
-		}
-		disposition, params, err := mime.ParseMediaType(mimePart.Header.Get("Content-Disposition"))
-		if err != nil {
-			log.Printf("Invalid Content-Disposition: %v", err)
-			break
+			log.Fatalln("Failed to create MultipartReader", err)
 		}
 
-		//Create File
-		f, err := os.OpenFile(params["filename"], os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer f.Close()
-	
-		_, err = f.Write(mimePart)
-		if err != nil {
-			log.Fatal(err)
-		}
+		for {
+			mimePart, err := multipart.NextPart()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Printf("Error reading multipart section: %v", err)
+				break
+			}
+			_, params, err := mime.ParseMediaType(mimePart.Header.Get("Content-Disposition"))
+			if err != nil {
+				log.Printf("Invalid Content-Disposition: %v", err)
+				break
+			}
 
-		
-	}
-	c.JSON(http.StatusCreated, gin.H{"message": "Processado sem erros."})
+			//Create File
+			f, err := os.OpenFile(params["filename"], os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer f.Close()
+
+			slurp, err := ioutil.ReadAll(mimePart)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			_, err = f.Write(slurp)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+		}
+		c.JSON(http.StatusCreated, gin.H{"message": "Processado sem erros."})
 	}
 }
