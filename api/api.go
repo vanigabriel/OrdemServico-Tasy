728553503d60c3c7cbb508553ab3540c3ad5b940
Gabriel Angelo Vani <vani.gabr@gmail.com>
2019-09-26 09:57:32 -0300

    Update api.go

--- api/api.go
+++ api/api.go
@@ -18,6 +18,7 @@ func SetupRouter(service *ordem.Service) *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
 
 	r.POST("/ordemservico", PostOS(service))
+	r.POST("/ordemservico/files", PostFiles)
 
 	return r
 }
@@ -42,44 +43,49 @@ func PostOS(service *ordem.Service) func(c *gin.Context) {
 			return
 		}
 
-		multipart, err := c.Request.MultipartReader()
-		if err != nil {
-			log.Fatalln("Failed to create MultipartReader", err)
-		}
-
-		for {
-			mimePart, err := multipart.NextPart()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				log.Printf("Error reading multipart section: %v", err)
-				break
-			}
-			_, params, err := mime.ParseMediaType(mimePart.Header.Get("Content-Disposition"))
-			if err != nil {
-				log.Printf("Invalid Content-Disposition: %v", err)
-				break
-			}
-
-			//Create File
-			f, err := os.OpenFile(params["filename"], os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer f.Close()
-
-			slurp, err := ioutil.ReadAll(mimePart)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			_, err = f.Write(slurp)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-		}
 		c.JSON(http.StatusCreated, gin.H{"message": "Processado sem erros."})
 	}
+}
+
+func PostFiles(c *gin.Context) {
+	multipart, err := c.Request.MultipartReader()
+	if err != nil {
+		log.Fatalln("Failed to create MultipartReader", err)
+	}
+
+	for {
+		mimePart, err := multipart.NextPart()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Printf("Error reading multipart section: %v", err)
+			break
+		}
+		_, params, err := mime.ParseMediaType(mimePart.Header.Get("Content-Disposition"))
+		if err != nil {
+			log.Printf("Invalid Content-Disposition: %v", err)
+			break
+		}
+
+		//Create File
+		f, err := os.OpenFile(params["filename"], os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		slurp, err := ioutil.ReadAll(mimePart)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = f.Write(slurp)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Processado sem erros."})
 }
